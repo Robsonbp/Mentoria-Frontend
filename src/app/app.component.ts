156af63e21285cf1071174388c2c227eb2498dbb
Robson Pinto <robsonbpinto@outlook.com>
2024-03-15 21:27:49 -0300

    Primeiro commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PessoaService } from './services/pessoa.service';
+import { Pessoa } from './models/pessoa';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'mentoria-frontend';
+
+  pessoa = {} as Pessoa;
+  pessoas: Pessoa[];
+
+  constructor(private pessoaService: PessoaService) {}
+  
+  ngOnInit() {
+    this.getPessoas();
+  }
+
+  // Chama o serviço para obtém todos os carros
+  getPessoas() {
+    this.pessoaService.getPessoas().subscribe((pessoas: Pessoa[]) => {
+      this.pessoas = pessoas;
+    });
+  }
 }
---
+++ src/app/services/pessoa.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { retry, catchError } from 'rxjs/operators';
+import { Pessoa } from '../models/pessoa';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PessoaService {
+
+  url = 'http://localhost:6000/pessoas/'
+
+  constructor(private httpClient: HttpClient) { }
+
+  // Headers
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  }
+
+  // Obtem todas as pessoas
+  getPessoas(): Observable<Pessoa[]> {
+    return this.httpClient.get<Pessoa[]>(this.url)
+      .pipe(
+        retry(2),
+        catchError(this.handleError))
+  }
+
+  // Manipulação de erros
+  handleError(error: HttpErrorResponse) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      // Erro ocorreu no lado do client
+      errorMessage = error.error.message;
+    } else {
+      // Erro ocorreu no lado do servidor
+      errorMessage = `Código do erro: ${error.status}, ` + `menssagem: ${error.message}`;
+    }
+    console.log(errorMessage);
+    return throwError(errorMessage);
+  };
+}
